7ebc4e2782e04fe8fbf04a8100c7e02edcac002f
Miodrag Milanovic <mmicko@gmail.com>
2015-05-20 18:44:58 +0200

    put back old python (nw)

--- vendor/python/Scripts/easy_install-2.7-script.py
+++ vendor/python/Scripts/easy_install-2.7-script.py
@@ -1,10 +1,10 @@
-#!C:\buildtools\vendor\python\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==7.0','console_scripts','easy_install-2.7'
-__requires__ = 'setuptools==7.0'
+#!python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==1.0','console_scripts','easy_install-2.7'
+__requires__ = 'setuptools==1.0'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('setuptools==7.0', 'console_scripts', 'easy_install-2.7')()
+        load_entry_point('setuptools==1.0', 'console_scripts', 'easy_install-2.7')()
     )
--- vendor/python/Scripts/easy_install-script.py
+++ vendor/python/Scripts/easy_install-script.py
@@ -1,10 +1,10 @@
-#!C:\buildtools\vendor\python\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==7.0','console_scripts','easy_install'
-__requires__ = 'setuptools==7.0'
+#!python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==1.0'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('setuptools==7.0', 'console_scripts', 'easy_install')()
+        load_entry_point('setuptools==1.0', 'console_scripts', 'easy_install')()
     )
